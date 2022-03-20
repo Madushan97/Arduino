8046dba0c87940ee0aeb14d5d2d41683e7903f13
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-03-20 10:56:12 +0530

    system 11 counts 5 presses using an alternative approach

---
+++ C/CounterBasedTrigger- counts 5 presses using sys 11/CounterBasedTrigger- counts 5 presses using sys 11/main.c
@@ -0,0 +1,32 @@
+/*
+ * CounterBasedTrigger- counts 5 presses using sys 11.c
+ *
+ * Created: 3/20/2022 9:37:24 AM
+ * Author : Madushan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    DDRB = DDRB | 0b00100000;		// config PB5 as output
+	DDRD = DDRD & 0b11101111;		// config PD4 as input
+	
+	PORTD = PORTD | 0b00010000;		// pull-up PD4
+	
+	TCCR0B = TCCR0B | 0b00000110;	// clock source as external
+	TCNT0 = 251;
+	
+    while (1) 
+    {
+		if( TIFR0 & 0b00000001 ){
+			PORTB = PORTB ^ 0b00100000;		// toggle PB5
+			TCNT0 = 251;
+			TIFR0 = TIFR0 | 0b00000001;		// clear the timer 0 overflow bit
+		}
+		
+    }
+	return 0;
+}
+
